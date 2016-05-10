51ae4c6fbe0decdfefdedb5d08ae1f16669384c1
deac <deac@sdicg.com>
2016-05-09 21:01:35 -0400

    updating lcd-show storage location

--- lcd_show.sh
+++ lcd_show.sh
@@ -1,17 +1,17 @@
 install_LCD_show () {
   echo_start
   echo -n "${gold}Downloading LCD-show${default}"
-  wget -O ~/LCD-show-151102.tar.gz http://www.spotpear.com/download/diver24-5/LCD-show-151102.tar.gz  > /dev/null 2>&1
+  wget -O /opt/LCD-show-151102.tar.gz http://www.spotpear.com/download/diver24-5/LCD-show-151102.tar.gz  > /dev/null 2>&1
   test_for_success $?
 
   echo_start
   echo -n "${gold}Unziping LCD-show to /opt/LCD-show${default}"
-  tar xvf ~/LCD-show-151102.tar.gz --directory /opt/LCD-show > /dev/null 2>&1
+  tar xvf /opt/LCD-show-151102.tar.gz --directory /opt > /dev/null 2>&1
   test_for_success $?
 
   echo_start
   echo -n "${gold}Cleaning up install files${default}"
-  rm ~/LCD-show-151102.tar.gz > /dev/null 2>&1
+  rm /opt/LCD-show-151102.tar.gz > /dev/null 2>&1
   test_for_success $?
 }
 
