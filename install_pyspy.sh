81603a7624977e5a7b5abe3e7600792cd6d28679
Deac <deac.karns@gmail.com>
2017-01-18 22:07:55 -0500

    Update to simpler logging script

--- install_pyspy.sh
+++ install_pyspy.sh
@@ -1,22 +1,35 @@
+echo_start
+echo -n "${gold}Downloading gps driver and utilities${default}"
 sudo apt-get install gpsd gpsd-clients python-gps
+test_for_success $?
 
-#show the cgps terminal to confirm functionality
-#cgps
-
-su pi
 #clone the py_spy.py script to the home directory
+echo_start
+echo -n "${gold}Cloning python gps script into ~/pyspy${default}"
 git clone https://github.com/peledies/pyspy.git /home/pi/pyspy
+test_for_success $?
 
+echo_start
+echo -n "${gold}Creating ~/pypsy/track.json${default}"
 touch /home/pi/pyspy/track.json
+test_for_success $?
 
-echo "[]" > /home/pi/pyspy/track.json
+echo_start
+echo -n "${gold}Modifying permissions of pyspy${default}"
+sudo chown -R pi:pi /home/pi/pyspy
+sudo chmod 755 -R /home/pi/pyspy
+test_for_success $?
 
-# enable the py_spy script to run on boot
-
+echo_start
+echo -n "${gold}Adding line to root crontab to start logging data on boot${default}"
 #write out current crontab
 crontab -l > ohmycron
 #echo new cron into cron file
-echo "@reboot /usr/bin/python /home/pi/pyspy/pyspy.py 2>&1"  >> ohmycron
+echo "@reboot gpspipe -w | grep lat >> /home/pi/pyspy/track.json 2>&1"  >> ohmycron
 #install new cron file
 crontab ohmycron
 rm ohmycron
+test_for_success $?
+
+echo -n "${gold}You can type ${cyan}cgps${gold} to get an instant view of the gps data${default}"
+
